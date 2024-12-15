2c919ad54fd08781d7c17039637b40c6187a9b42
prabhatgrg <prabhatgurung34@gmail.com>
2024-12-15 19:49:59 +0545

    feat: Changed Edit Notes Screen to have TextInput instead of only Text. Also added button and changed the overall layout to be centered

--- app/note/[id].tsx
+++ app/note/[id].tsx
@@ -1,20 +1,50 @@
 import React from "react";
 import { useRouter, useLocalSearchParams } from "expo-router";
 import { useState, useContext } from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, View, Dimensions } from "react-native";
+import { ThemedView } from "@/components/ThemedView";
 import { NoteContext } from "../NoteContext";
+import { Button, TextInput } from "react-native-paper";
 
 const Note = () => {
+  const [note, setNote] = useState<string>("");
   const { id, content } = useLocalSearchParams();
+  // const {} = useContext(NoteContext);
   return (
     <>
-      <SafeAreaView>
-        {/* <Text>{id}</Text> */}
-        <Text>{content}</Text>
-      </SafeAreaView>
+      <ThemedView style={styles.container}>
+        {/* <View style={styles.textInput}> */}
+        <TextInput
+          mode="outlined"
+          multiline={true}
+          style={styles.textInput}
+          contentStyle={styles.textInputContent}
+          onChangeText={setNote}
+        >
+          {content}
+        </TextInput>
+        <Button mode="contained">Edit</Button>
+        {/* </View> */}
+      </ThemedView>
     </>
   );
 };
+
+const width = Dimensions.get("window").width - 50;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  textInput: {
+    width: width,
+  },
+  textInputContent: {
+    textAlignVertical: "auto"
+  }
+});
 
 export default Note;
